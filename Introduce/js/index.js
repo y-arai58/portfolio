09cdf4430de0453b49bccf264521a8a7e7a13085
y-arai58 <y.arai.dev222@gmail.com>
2025-05-21 22:52:55 +0900

    ファイル移動

--- Introduce/js/index.js
+++
@@ -1,4 +0,0 @@
-window.onload = function () {
-  const mvLine = document.querySelector('.mv-line span');
-  mvLine.classList.add('active');
-};
--- animation/index.js
+++
@@ -1,6 +0,0 @@
-new Vivus('scratch', {
-  type: 'oneByOne',
-  duration: 500,
-  animTimingFunction: Vivus.EASE_OUT,
-  start: 'autostart',
-});
