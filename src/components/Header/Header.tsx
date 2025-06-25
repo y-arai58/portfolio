3db44c5d5b47879382b58e6b11251652a33a26e6
y-arai58 <y.arai.dev222@gmail.com>
2025-06-25 20:49:20 +0900

    ビルド

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -55,10 +55,7 @@ export const Header: React.FC<HeaderProps> = ({ setTab }) => {
               href='#top'
               onClick={handleTopClick}
             >
-              <img
-                className='headerIcon'
-                src='https://y-arai58.github.io/portfolio/assets/ushimikan.jpg'
-              />
+              <img className='headerIcon' src='/ushimikan.jpg' />
               <p className='headerName'>yui arai</p>
             </a>
             <p
